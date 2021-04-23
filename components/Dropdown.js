04363d55d797b8eb47dde50a5374161299ee34aa
Alexey Rudoy <ao.rudoy@yandex.ru>
2021-04-23 23:20:33 +0300

    feature: add sections: faq/footer/popup/

--- components/Dropdown.js
+++ components/Dropdown.js
@@ -1,8 +1,6 @@
-class Dropdown {
+export default class Dropdown {
   constructor(dropdownSelector) {
-    // this._dropdownType = document.querySelectorAll(dropdownSelector);
-    this._dropdownList = Array.from(document.querySelectorAll(dropdownSelector))
-    console.log(this._dropdownList)
+    this._dropdownList = Array.from(document.querySelectorAll(dropdownSelector));
   }
 
   _toggle(dropdownCell) {
@@ -11,12 +9,7 @@ class Dropdown {
 
   setEventListeners() {
     this._dropdownList.forEach((dropdownCell) => {
-      console.log(dropdownCell)
       dropdownCell.addEventListener("click", () => this._toggle(dropdownCell))
     })
   }
 }
-
-const dropdown1 = new Dropdown('.faq__dropdown-cell');
-
-dropdown1.setEventListeners();
