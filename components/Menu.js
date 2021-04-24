61764fd9dd30c6a68bacbd9924c7ce9997b54de5
Khan Tagiev <xan.2011@yandex.ru>
2021-04-24 16:53:57 +0300

    feat: nav menu change with scroll

--- components/Menu.js
+++ components/Menu.js
@@ -1,13 +1,14 @@
 export default class Menu {
-  constructor({menuSelector, openSelector}) {
+  constructor({menuSelector,menuLinkSelector, openSelector}) {
     this._menuElement = document.querySelector(menuSelector);
+    this._menuLinks = Array.from(this._menuElement.querySelectorAll(menuLinkSelector))
     this._openSelector = openSelector
     this._handleClickClose = this._handleClickClose.bind(this);
   }
 
   _handleClickClose(evt) {
     console.log(evt.target)
-    if (evt.target.classList.contains('menu__link')|| evt.target.classList.contains('menu__link-container') || evt.target.classList.contains('menu')) {
+    if (evt.target.classList.contains('menu__link')|| evt.target.classList.contains('menu__container') || evt.target.classList.contains('menu')) {
       this.close()
      };
   }
@@ -20,5 +21,17 @@ export default class Menu {
   close() {
     this._menuElement.classList.remove(this._openSelector);
     document.removeEventListener("click", this._handleClickClose);
+  }
+
+  clearMenuLinks (id) {
+    if (this._menuLinks[id].classList.contains('menu__link_active')) {
+
+    } else {
+      this._menuLinks.forEach(item => {item.classList.remove('menu__link_active')})
+    }
+  }
+
+  returnMenuLinks() {
+    return this._menuLinks
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,18 @@
 // TODO webpack import './index.css';
 
-// import Portfolio from "../components/Portfolio.js";
-// import Project from "../components/Project.js";
+import Portfolio from "../components/Portfolio.js";
+import Project from "../components/Project.js";
 import {projects, menuBtn} from "../utils/constants.js";
 import Dropdown from "../components/Dropdown.js";
 import FormValidator from "../components/FormValidator.js"
 import PopupWithForm from '../components/PopupWithForm.js'
 import Menu from '../components/Menu.js'
 
-// const portfolio = new Portfolio({selector: '.portfolio'});
-// projects.forEach(projectData => {
-//   const project = new Project({templateSelector: ".project-template", ...projectData});
-//   portfolio.addProject(project.generateElement());
-// })
+const portfolio = new Portfolio({selector: '.portfolio'});
+projects.forEach(projectData => {
+  const project = new Project({templateSelector: ".project-template", ...projectData});
+  portfolio.addProject(project.generateElement());
+})
 
 const validationConfig = {
   formSelector: ".popup__form",
@@ -52,7 +52,41 @@ addButtonMesto.addEventListener("click", () => {
 
 const menu = new Menu ({
   menuSelector: '.menu__container',
+  menuLinkSelector: '.menu__link',
   openSelector: 'menu__container_active'
 })
 
+const links = menu.returnMenuLinks()
+
+function changeMenuWithScroll() {
+  const intro = document.querySelector('#intro').offsetTop;
+  const about = document.querySelector('#about').offsetTop;
+  const skills = document.querySelector('#skills').offsetTop;
+  const portfolio = document.querySelector('#portfolio').offsetTop;
+  const faq = document.querySelector('#faq').offsetTop;
+  const footer = document.querySelector('#footer').offsetTop;
+  const scrollY = window.pageYOffset;
+
+  if (scrollY < about  - 20 ) {
+    menu.clearMenuLinks(0)
+    links[0].classList.add('menu__link_active')
+  } else if (scrollY < skills  - 20 ) {
+    menu.clearMenuLinks(1)
+    links[1].classList.add('menu__link_active')
+  } else if (scrollY < portfolio  - 20 ) {
+    menu.clearMenuLinks(2)
+    links[2].classList.add('menu__link_active')
+  } else if (scrollY < faq  - 20 ) {
+    menu.clearMenuLinks(3)
+    links[3].classList.add('menu__link_active')
+  } else if (scrollY < footer - 50) {
+    menu.clearMenuLinks(4)
+    links[4].classList.add('menu__link_active')
+  } else{
+    menu.clearMenuLinks(5)
+    links[5].classList.add('menu__link_active')
+  }
+}
+
+document.addEventListener('scroll', changeMenuWithScroll)
 menuBtn.addEventListener('click', () => {menu.open()})
